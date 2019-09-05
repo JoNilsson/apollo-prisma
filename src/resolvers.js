0069a4452a2a16c142921e8dd5865749ef0e4b63
Johanness Nilsson <jnilsson@teknomadix.com>
2019-09-05 15:52:26 -0700

    jwt token

--- src/resolvers.js
+++ src/resolvers.js
@@ -2,8 +2,11 @@ const bcrypt = require('bcryptjs')
 
 const resolvers = {
   Mutation: {
+    // fetch username and password from arguments
     register: async (parent, { username, password }, ctx, info) => {
+      // hash the password using the bcryptjs
       const hashedPassword = await bcrypt.hash(password, 10)
+      // store detail in Prisma and return a user
       const user = await ctx.prisma.createUser({
         username,
         password: hashedPassword
