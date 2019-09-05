257eaead850b8fedc927be521c6d3a87c41292c2
Johanness Nilsson <jnilsson@teknomadix.com>
2019-09-05 16:09:41 -0700

    issue jwt token

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const resolvers = {
   Mutation: {
@@ -12,6 +13,34 @@ const resolvers = {
         password: hashedPassword
       })
       return user
+    },
+    login: async (parent, { username, password }, ctx, info) => {
+      const user = await ctx.prisma.user({ username })
+
+      if (!user) {
+        throw new Error('Invalid Login')
+      }
+
+      const passwordMatch = await bcrypt.compare(password, user.password)
+
+      if (!passwordMatch) {
+        throw new Error('Invalid Login')
+      }
+
+      const token = jwt.sign(
+        {
+          id: user.id,
+          username: user.email
+        },
+        'my-secret-from-env-file-in-prod',
+        {
+          expiresIn: '1d' // token life
+        }
+      )
+      return {
+        token,
+        user
+      }
     }
   }
 }
